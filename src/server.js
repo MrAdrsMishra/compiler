5c74ef629453177c5cd6bf7490bf5cbeda4cfabd
Mr_Adrs <adrshmishra018@gmail.com>
2026-02-08 12:07:22 +0530

    deployment modification for aws EC2

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { app } from "./app.js";
 import dotenv from 'dotenv'
 
@@ -9,8 +10,9 @@ try {
     app.listen(process.env.PORT,()=>{
         console.log(`main server started at port: ${process.env.PORT}`)
     })
-    app.get('/',(req,res)=>{
-        res.send(`the server is listening here`)
+    app.get('/',async(req,res)=>{
+        let response=await axios.get(`${process.env.RUNNER_URL}/health`)
+       res.send(response)
     })
 } catch (error) {
     console.error(`somthing went wrong while starting server`,error)
